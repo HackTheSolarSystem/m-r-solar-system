e51fe6a671c47271ce9bb22c814443272bc2af73
nishantmehtan <nishantmehta.n@gmail.com>
2019-02-09 19:30:44 -0500

    updated questions on backend

--- backend-server/routes/questions.js
+++ backend-server/routes/questions.js
@@ -4,11 +4,9 @@ var router = express.Router();
 
 router.get('/:questionNumber', function(req, res, next) {
     if(req.params.questionNumber == 1)
-        res.json({ question: 'Which of these metals occurs in earths core [one]', options: ['iron', 'gold', 'nickel']});
+        res.json({ question: 'Which of these metals occurs in earths core', options: ['iron', 'gold', 'nickel'], answer: 'iron'});
     if(req.params.questionNumber == 2)
-        res.json({ question: 'Which of these metals occurs in earths core [two]', options: ['iron', 'gold', 'nickel']});
-    if(req.params.questionNumber == 3)
-        res.json({ question: 'Which of these metals occurs in earths core [three]', options: ['iron', 'gold', 'nickel']});
+        res.json({ question: 'What percentage of Earth\'s atmosphere is made up of Nitrogen?' , options: ['10%', '30%', '78%'], answer:'78%'});
 });
 
 module.exports = router;
